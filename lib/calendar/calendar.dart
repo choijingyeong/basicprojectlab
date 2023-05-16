0f67ef64e663a8bb7d2a8e925be2dc7ba5e74195
YIMSEBIN <jij09123@gmail.com>
2023-05-16 15:33:33 +0900

    강윤수 분량 추가 완료

--- lib/calendar/calendar.dart
+++ lib/calendar/calendar.dart
@@ -1,17 +1,107 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class Calendar extends StatefulWidget {
+final images = [
+  'assets/images/milky.jpg',
+  'assets/images/mont.jpg',
+  'assets/images/tower.jpg',
+  'assets/images/woman.jpg',
+];
+
+class Calendar extends StatelessWidget {
   const Calendar({Key? key}) : super(key: key);
 
   @override
-  _CalendarState createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+              elevation: 1.0,
+              title: Text('RemindDiary', style: TextStyle(color: Colors.black),),
+              backgroundColor: Colors.white,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {  },
+                  color: Colors.black,
+                )
+              ]
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: TableCalendar(
+                  firstDay: DateTime.utc(2023, 01, 01),
+                  lastDay: DateTime.utc(2023, 12, 31),
+                  focusedDay: DateTime.now(),
+                  eventLoader: (day){
+                    if(day.day==5) {
+                      return ['event'];
+                    }
+                    return [];
+                  },
+                  headerStyle: HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  calendarStyle: CalendarStyle(
+                    markerDecoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 3.0,
+                          spreadRadius: 1.0,
+                        ),
+                      ],
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/tower.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      color: Colors.amberAccent,
+                      borderRadius: BorderRadius.circular(7.0),
+                    ),
+                    markerSize: 40.0,
+                    markersAlignment: Alignment.topCenter,
+                    outsideDaysVisible: false,
+                  ),
+                ),
+              ),
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 250.0,
+                ),
+                items: images.map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.fromLTRB(10, 40, 10, 0),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 10.0,
+                                spreadRadius: 2.0,
+                              )
+                            ]
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: Image.asset(
+                            i,
+                            fit:BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+        )
+    );
   }
-
 }
--- lib/userpage/user_page.dart
+++ lib/userpage/user_page.dart
@@ -1,17 +1,176 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class UserPage extends StatefulWidget {
+final images = [
+  'assets/images/milky.jpg',
+  'assets/images/mont.jpg',
+  'assets/images/tower.jpg',
+  'assets/images/woman.jpg',
+];
+
+class UserPage extends StatelessWidget {
   const UserPage({Key? key}) : super(key: key);
 
-  @override
-  _UserPageState createState() => _UserPageState();
-}
+  SideTitles get _bottom => SideTitles(
+      showTitles: true,
+      interval: 1.0,
 
-class _UserPageState extends State<UserPage> {
+      getTitlesWidget: (value, meta) {
+        String text = '${value.toInt()}일';
+        return Text(text);
+      }
+
+  );
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            elevation: 1.0,
+            title: Text('RemindDiary', style: TextStyle(color: Colors.black),),
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {  },
+                color: Colors.black,
+              ),
+            ]
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Text("감정 그래프", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 10, 0, 30),
+              height: 250.0,
+              width: 375.0,
+              child: LineChart(
+                LineChartData(
+                    backgroundColor: Colors.black12,
+                    borderData: FlBorderData(
+                      show: true,
+                      border: Border(
+                        left: BorderSide(color: Colors.transparent),
+                        right: BorderSide(color: Colors.transparent),
+                        top: BorderSide(width: 1),
+                        bottom: BorderSide(width: 1),
+                      ),
+                    ),
+                    gridData: FlGridData(
+                      show: true,
+                      drawVerticalLine: false,
+                      drawHorizontalLine: false,
+                    ),
+                    titlesData: FlTitlesData(
+                      topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      bottomTitles: AxisTitles(sideTitles: _bottom),
+                      leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    ),
+                    minX: 14,
+                    maxX: 19,
+                    minY: 0,
+                    maxY: 100,
+                    lineBarsData: [
+                      LineChartBarData(
+                          spots: [
+                            FlSpot(14,30),
+                            FlSpot(15,40),
+                            FlSpot(16,90),
+                            FlSpot(17,50),
+                            FlSpot(18, 20),
+                            FlSpot(19, 40),
+                          ]
+                      )
+                    ]
+                ),
+              ),
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 220.0,
+              ),
+              items: images.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.fromLTRB(10, 40, 10, 0),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 10.0,
+                              spreadRadius: 2.0,
+                            )
+                          ]
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10.0),
+                        child: Image.asset(
+                          i,
+                          fit:BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(5, 15, 5, 10),
+                  width: 60,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow:[ BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 3.0,
+                      spreadRadius: 1.0,
+                    ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text("봄", style: TextStyle(color: Colors.black, fontSize: 14,)),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(5, 15, 5, 10),
+                  width: 60,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(8.0),
+                    boxShadow:[ BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 3.0,
+                      spreadRadius: 1.0,
+                    ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Text("벚꽃", style: TextStyle(color: Colors.black, fontSize: 14,)),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: Text("동학사에 가서 꽃구경을 했다.", style: TextStyle(color: Colors.black, fontSize: 16,)),
+              margin: EdgeInsets.fromLTRB(100, 15, 100, 40),
+            ),
+          ], // childeren
+        ),
+      ),
+    );
   }
-
 }
