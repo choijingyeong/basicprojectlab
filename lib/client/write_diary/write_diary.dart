cddefedeec323f550e9ed2ea96d20ddc1e2e98b9
choijingyeong <dldeo9987@gmail.com>
2023-05-28 01:02:50 +0900

    11번, 12번 화면 구현 완료

--- lib/client/write_diary/write_diary.dart
+++ lib/client/write_diary/write_diary.dart
@@ -38,26 +38,18 @@ class staticVariables extends ChangeNotifier {
   List<Widget> _tagList = [];
 
   void addText(String txt) {
-    _tagList.add(
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
-          margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(
-                  5)),
-              color: Colors.grey
-          ),
-          child: Text(
-            "#"+txt,
-            style: TextStyle(
-                fontSize: 10,
-                color: Colors.white
-            ),
-          ),
-        )
-    );
+    _tagList.add(Container(
+      padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+      margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Colors.grey),
+      child: Text(
+        "#" + txt,
+        style: TextStyle(fontSize: 10, color: Colors.white),
+      ),
+    ));
   }
-
 }
 
 class _uploadNewPost extends State {
@@ -67,16 +59,11 @@ class _uploadNewPost extends State {
       padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
       margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
       decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(
-              5)),
-          color: Colors.grey
-      ),
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: Colors.grey),
       child: const Text(
         '#성년의 날',
-        style: TextStyle(
-            fontSize: 10,
-            color: Colors.white
-        ),
+        style: TextStyle(fontSize: 10, color: Colors.white),
       ),
     ),
     Container(
@@ -84,14 +71,10 @@ class _uploadNewPost extends State {
       margin: EdgeInsets.fromLTRB(10, 5, 10, 10),
       decoration: const BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(5)),
-          color: Colors.grey
-      ),
+          color: Colors.grey),
       child: Text(
         "#영화제",
-        style: const TextStyle(
-            fontSize: 10,
-            color: Colors.white
-        ),
+        style: const TextStyle(fontSize: 10, color: Colors.white),
       ),
     )
   ];
@@ -105,23 +88,16 @@ class _uploadNewPost extends State {
             ),
             title: Text(
               "태그 추가하기",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black
-              ),
+              style: TextStyle(fontSize: 15, color: Colors.black),
             ),
             actions: [
               TextButton(
                 onPressed: () {
-
                   Navigator.pop(context);
                 },
                 child: Text(
                   "추가",
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black
-                  ),
+                  style: TextStyle(fontSize: 15, color: Colors.black),
                 ),
               ),
               TextButton(
@@ -130,10 +106,7 @@ class _uploadNewPost extends State {
                   },
                   child: Text(
                     "닫기",
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black
-                    ),
+                    style: TextStyle(fontSize: 15, color: Colors.black),
                   ))
             ],
             content: TextField(
@@ -141,110 +114,100 @@ class _uploadNewPost extends State {
             ),
           );
         },
-        context: this.context
-    );
+        context: this.context);
   }
 
   int emotionIndex = 0;
-  List emotionArr = [Colors.red, Colors.orange, Colors.yellow, Colors.lightGreenAccent, Colors.lightBlueAccent];
+  List emotionArr = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.lightGreenAccent,
+    Colors.lightBlueAccent
+  ];
   Container emotionCircle = Container(
     width: 200,
     height: 200,
     alignment: Alignment.centerLeft,
     decoration: BoxDecoration(
-        shape: BoxShape.circle,
+      shape: BoxShape.circle,
       color: Colors.amberAccent,
     ),
   );
+
   void selectEmotion() {
     showDialog(
         builder: (BuildContext context) {
           return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            title: Text(
-              "감정 변경하기",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
               ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "적용",
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black
-                  ),
-                ),
+              title: Text(
+                "감정 변경하기",
+                style: TextStyle(fontSize: 15, color: Colors.black),
               ),
-              TextButton(
+              actions: [
+                TextButton(
                   onPressed: () {
                     Navigator.pop(context);
                   },
                   child: Text(
-                    "닫기",
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black
-                    ),
-                  ))
-            ],
-            content: Row(
-              children: [
-                TextButton(
-                    onPressed: () {
-                      emotionIndex = 0;
-                      print(emotionIndex);
-                    },
-                    child: Text(
-                      "화남",
-                    )
+                    "적용",
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
                 ),
                 TextButton(
                     onPressed: () {
-                      emotionIndex = 1;
-                      print(emotionIndex);
+                      Navigator.pop(context);
                     },
                     child: Text(
-                      "신남",
-                    )
-                ),
-                TextButton(
-                    onPressed: () {
-                      emotionIndex = 2;
-                    },
-                    child: Text(
-                      "기쁨",
-                    )
-                ),
-                TextButton(
-                    onPressed: () {
-                      emotionIndex = 3;
-                    },
-                    child: Text(
-                      "즐거움",
-                    )
-                ),
-                TextButton(
-                    onPressed: () {
-                      emotionIndex = 4;
-                    },
-                    child: Text(
-                      "슬픔",
-                    )
-                ),
+                      "닫기",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ))
               ],
-            )
-          );
+              content: Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        emotionIndex = 0;
+                        print(emotionIndex);
+                      },
+                      child: Text(
+                        "화남",
+                      )),
+                  TextButton(
+                      onPressed: () {
+                        emotionIndex = 1;
+                        print(emotionIndex);
+                      },
+                      child: Text(
+                        "신남",
+                      )),
+                  TextButton(
+                      onPressed: () {
+                        emotionIndex = 2;
+                      },
+                      child: Text(
+                        "기쁨",
+                      )),
+                  TextButton(
+                      onPressed: () {
+                        emotionIndex = 3;
+                      },
+                      child: Text(
+                        "즐거움",
+                      )),
+                  TextButton(
+                      onPressed: () {
+                        emotionIndex = 4;
+                      },
+                      child: Text(
+                        "슬픔",
+                      )),
+                ],
+              ));
         },
-        context: this.context
-    );
+        context: this.context);
   }
 
   @override
@@ -263,53 +226,43 @@ class _uploadNewPost extends State {
                         emotionCircle,
                         Container(
                             child: CarouselSlider(
-                              options: CarouselOptions(
-                                height: 250.0,
-                              ),
-                              items: images.map((i) {
-                                return Builder(
-                                  builder: (BuildContext context) {
-                                    return Container(
-                                      width: MediaQuery
-                                          .of(context)
-                                          .size
-                                          .width,
-                                      margin: EdgeInsets.fromLTRB(
-                                          10, 40, 10, 0),
-                                      decoration: BoxDecoration(
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.black26,
-                                              blurRadius: 10.0,
-                                              spreadRadius: 2.0,
-                                            )
-                                          ]
-                                      ),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(
-                                            10.0),
-                                        child: Image.asset(
-                                          i,
-                                          fit: BoxFit.cover,
-                                        ),
-                                      ),
-                                    );
-                                  },
+                          options: CarouselOptions(
+                            height: 250.0,
+                          ),
+                          items: images.map((i) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: EdgeInsets.fromLTRB(10, 40, 10, 0),
+                                  decoration: BoxDecoration(boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black26,
+                                      blurRadius: 10.0,
+                                      spreadRadius: 2.0,
+                                    )
+                                  ]),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Image.asset(
+                                      i,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
                                 );
-                              }).toList(),
-                            )
-                        ),
+                              },
+                            );
+                          }).toList(),
+                        )),
                       ],
                     ),
                     Container(
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: tagList,
-                        )
-                    )
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: tagList,
+                    ))
                   ],
-                )
-            ),
+                )),
             Expanded(
                 flex: 5,
                 child: Column(
@@ -321,21 +274,16 @@ class _uploadNewPost extends State {
                           children: [
                             Text(
                               "오늘은 영화제에서 영화제를 봤다",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black
-                              ),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.black),
                             ),
                             Text(
                               "어바웃 타임은 처음 봤는데 재밌었다.",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black
-                              ),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.black),
                             )
                           ],
-                        )
-                    ),
+                        )),
                     Expanded(
                         flex: 3,
                         child: Row(
@@ -344,42 +292,32 @@ class _uploadNewPost extends State {
                             TextButton(
                                 onPressed: () {
                                   addTag();
-
                                 },
                                 child: Text(
                                   "태그추가",
                                   style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black
-                                  ),
-                                )
-                            ),
+                                      fontSize: 15, color: Colors.black),
+                                )),
                             TextButton(
-                                onPressed: () { selectEmotion(); },
-                                child: Text(
-                                  "감정변경",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Colors.black
-                                  ),
-                                ),),
+                              onPressed: () {
+                                selectEmotion();
+                              },
+                              child: Text(
+                                "감정변경",
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.black),
+                              ),
+                            ),
                             Text(
                               "저장",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black
-                              ),
+                              style:
+                                  TextStyle(fontSize: 15, color: Colors.black),
                             )
                           ],
-
-                        )
-                    )
+                        ))
                   ],
-                )
-            )
+                ))
           ],
-        )
-    );
+        ));
   }
 }
-
