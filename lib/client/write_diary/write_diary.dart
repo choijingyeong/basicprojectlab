5535736eb44c4eb59352c5ce5811000180af1b83
choijingyeong <dldeo9987@gmail.com>
2023-05-28 16:54:52 +0900

    커밋하래요...

--- lib/client/write_diary/write_diary.dart
+++ lib/client/write_diary/write_diary.dart
@@ -11,6 +11,26 @@ final images = [
   'images/movie2.png',
   'images/movie3.png',
 ];
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WriteDiary(),
+    );
+  }
+}
 
 class WriteDiary extends StatefulWidget {
   const WriteDiary({super.key});
